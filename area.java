ea0b7903746c189ce1ca33e2e93cdb44e24562ee
FAZIL AHAMMED.N <130854953+Fazil0018@users.noreply.github.com>
2024-10-08 12:30:51 +0530

    Create area.java
    It seems like you're trying to calculate the area of a circle, where d is presumably the diameter, and you're using 22/7 as an approximation for π (pi).
    
    However, there's a small issue in the calculation: 22/7 is being treated as integer division in Java, which results in 3 instead of the more precise 3.142857. To fix this, you should use 22.0/7 or Math.PI for better precision

---
+++ area.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class area{
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        int d=sc.nextInt();
+        double k=(22/7)*d*d;
+        System.out.printf("%.0f",k);
+    }
+}
